41e9ed23e8991de83f29a91b88fb7d588660088f
Michael Vaganov <mvaganov@hotmail.com>
2025-02-09 01:11:26 -0800

    refactor client/server logic out of generic client/server process

--- Program7.cs
+++ Program7.cs
@@ -16,14 +16,13 @@ namespace networkingMinimal {
 			Program p = new Program();
 			int port = defaultPort;
 			string host = localhost;
-			bool canBeServer = !CheckPortIsUsed(port);
+			bool canBeServer = !IsPortUsed(port);
 			Task t = p.Run(host, port, canBeServer);
-			// not allowed to await in Main, so we do a blocking sleep.
+			// main loop. not allowed to await in Main, so we do a blocking sleep.
 			while (!t.IsCompleted) { Thread.Sleep(1); }
-			Console.WriteLine($"finished {host}:{port}");
 		}
 
-		private static bool CheckPortIsUsed(int port) {
+		private static bool IsPortUsed(int port) {
 			IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
 			IPEndPoint[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpListeners();
 			foreach (IPEndPoint endpoint in tcpConnInfoArray) {
@@ -32,79 +31,6 @@ namespace networkingMinimal {
 				}
 			}
 			return false;
-		}
-
-		public async Task Run(string host, int port, bool createServer) {
-			if (createServer) {
-				await CreateEchoServer(port);
-			} else {
-				await CreateClient(host, port);
-			}
-		}
-
-		/// <param name="port"></param>
-		/// <returns>false if this port is already bound, or if there is some other server error</returns>
-		public static async Task<bool> CreateEchoServer(int port) {
-			IPEndPoint ipEndPoint = await GetEndPoint(localhost, port);
-			TcpListener listener = new TcpListener(ipEndPoint);
-			try {
-				listener.Start();
-			} catch {
-				return false; // will fail if a listener socket is already bound to the port
-			}
-			List<TcpClient> clients = new List<TcpClient>();
-			NetBuffer writeBuffer = new NetBuffer();
-			byte[] readBuffer = new byte[1024];
-			bool running = true, success = false;
-			string animation = "/-\\|";
-			int animationIndex = 0;
-			int whenToAnimateNext = Environment.TickCount;
-			try {
-				Console.WriteLine("listening at " + ipEndPoint);
-				while (running) {
-					Console.Write("\r");
-					TcpClient? handler = await ServiceClientsWhileListening(listener, IsServerGood, async () => {
-						await Read(clients, writeBuffer, readBuffer);
-						await writeBuffer.WriteFlush(clients);
-					});
-					if (handler == null) {
-						running = false;
-						Console.WriteLine("ended");
-						break;
-					}
-					NetworkStream stream = handler.GetStream();
-					Console.WriteLine("connected to new client " + handler.Client.RemoteEndPoint);
-					clients.Add(handler);
-					// example serialization. TODO make this in a delegate, with the corresponding delegate for reading.
-					var message = $"DateTime: {DateTime.Now}";
-					var dateTimeBytes = Encoding.UTF8.GetBytes(message);
-					await stream.WriteAsync(dateTimeBytes);
-
-					Console.WriteLine($"Sent message: \"{message}\"");
-				}
-				success = true;
-			} catch (Exception e) {
-				Console.WriteLine("SERVER ERROR\n" + e);
-				success = false;
-			} finally {
-				listener.Stop();
-			}
-			bool IsServerGood() {
-				if (Console.KeyAvailable) {
-					ConsoleKeyInfo keyInfo = Console.ReadKey();
-					if (keyInfo.Key == ConsoleKey.Escape) {
-						Console.WriteLine("~\n"); // needed because the console eats characters
-						return false;
-					}
-				}
-				if (Environment.TickCount >= whenToAnimateNext) {
-					Console.Write(animation[animationIndex++] + "\r");
-					if (animationIndex >= animation.Length) { animationIndex = 0; }
-					whenToAnimateNext += 200;
-				}
-				return true;
-			}
-			return success;
 		}
 		public static async Task<IPEndPoint> GetEndPoint(string host, int port) {
 			IPAddress ipAddress = await GetIpAddress(host);
@@ -115,88 +41,212 @@ namespace networkingMinimal {
 			IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync(host);
 			return ipHostInfo.AddressList[0];
 		}
-		private static async Task Read(List<TcpClient> clients, NetBuffer buff, byte[] networkInputBuffer) {
-			for (int i = clients.Count - 1; i >= 0; --i) {
-				TcpClient c = clients[i];
-				if (IsClearedDeadStream(c, i, clients)) { continue; }
-				await buff.Read(c, networkInputBuffer);
+
+		public async Task Run(string host, int port, bool createServer) {
+			if (createServer) {
+				ServerImplementation server = new ServerImplementation();
+				await ServerProcess(port, server.OnConnect, server.IsGood, server.Update);
+			} else {
+				ClientImplementation client = new ClientImplementation();
+				await ClientProcess(host, port, client.OnConnect, client.IsGood, client.Update);
 			}
 		}
-		private static async Task<TcpClient?> ServiceClientsWhileListening(TcpListener listener,
-		Func<bool> shouldContinue, Func<Task> whatToDoWhileWaiting) {
+
+		/// <param name="port"></param>
+		/// <returns>false if this port is already bound, or if there is some other server error</returns>
+		public static async Task<bool> ServerProcess(int port,
+		Func<TcpClient,Task> onConnect, Func<bool> isRunning, Func<Task> update) {
+			IPEndPoint ipEndPoint = await GetEndPoint(localhost, port);
+			TcpListener listener = new TcpListener(ipEndPoint);
+			try {
+				listener.Start();
+			} catch {
+				return false; // will fail if a listener socket is already bound to the port
+			}
+			bool running = true, success;
+			try {
+				Console.WriteLine("listening at " + ipEndPoint);
+				while (running) {
+					TcpClient? newClient = await UpdateWhileListening(listener, isRunning, update);
+					if (newClient == null) {
+						running = false;
+						break;
+					}
+					await onConnect?.Invoke(newClient);
+				}
+				success = true;
+			} catch (Exception e) {
+				Console.WriteLine("SERVER ERROR\n" + e);
+				success = false;
+			} finally {
+				listener.Stop();
+			}
+			return success;
+		}
+		private static async Task<TcpClient?> UpdateWhileListening(TcpListener listener,
+		Func<bool> IsRunning, Func<Task> Update) {
 			Task<TcpClient> tcpClientTask = listener.AcceptTcpClientAsync();
 			while (!tcpClientTask.IsCompleted) {
-				if (!shouldContinue.Invoke()) {
+				if (!IsRunning.Invoke()) {
 					return null;
 				}
-				await whatToDoWhileWaiting?.Invoke();
+				await Update?.Invoke();
 			}
 			return tcpClientTask.Result;
 		}
-		public static async Task CreateClient(string host, int port) {
-			IPEndPoint ipEndPoint = await GetEndPoint(host, port);
-			if (ipEndPoint == null) {
-				throw new Exception($"unable to connect to {host}:{port}");
-			}
-			ClientCommunicator cc = new ClientCommunicator();
+		public static async Task ClientProcess(string host, int port, Func<TcpClient, Task> onConnect, 
+		Func<bool> isRunning, Func<Task> update) {
 			try {
-				await cc.ConnectAsync(ipEndPoint);
-				StringBuilder stringBuilder = new StringBuilder();
-				while (cc.client.Connected) {
-					if (Console.KeyAvailable) {
-						ConsoleKeyInfo keyInfo = Console.ReadKey();
-						switch (keyInfo.Key) {
-							case ConsoleKey.Enter:
-								if (!cc.stream.CanWrite) {
-									Console.WriteLine($"can't write {string.Join(" ", cc.consoleInput)}");
-								} else if (cc.consoleInput.Count > 0) {
-									byte[] outbytes = new byte[cc.consoleInput.Count];
-									for (int i = 0; i < cc.consoleInput.Count; ++i) {
-										outbytes[i] = (byte)cc.consoleInput[i];
-									}
-									await cc.stream.WriteAsync(outbytes);
-									cc.stream.Flush();
-								}
-								cc.consoleInput.Clear();
-								break;
-							default:
-								if (keyInfo.KeyChar != 0) {
-									cc.consoleInput.Add(keyInfo.KeyChar);
-								}
-								break;
-						}
-					}
-					stringBuilder.Clear();
-					if (cc.stream.CanRead && cc.receivedTask.IsCompleted) {
-						int received = cc.receivedTask.Result;
-						Console.WriteLine($"received {received}");
-						if (received <= 0) { break; }
-						string message = Encoding.UTF8.GetString(cc.networkInputBuffer, 0, received);
-						Console.WriteLine(message);
-						stringBuilder.Append(message);
-						cc.receivedTask = cc.stream.ReadAsync(cc.networkInputBuffer);
-						Console.WriteLine("looking for more...");
-					}
-					if (stringBuilder.Length > 0) {
-						Console.WriteLine($"Message received: \"{stringBuilder}\"");
-					}
+				IPEndPoint ipEndPoint = await GetEndPoint(host, port);
+				if (ipEndPoint == null) {
+					throw new Exception($"unable to find {host}:{port}");
+				}
+				TcpClient client = new TcpClient();
+				await client.ConnectAsync(ipEndPoint.Address, ipEndPoint.Port);
+				if (!client.Connected) {
+					throw new Exception($"unable to connect to {host}:{port}");
+				}
+				await onConnect?.Invoke(client);
+				while (isRunning.Invoke()) {
+					await update.Invoke();
 				}
 			} catch (Exception e) {
-				Console.WriteLine(e);
+				Console.Error.WriteLine(e);
 			}
 			Console.WriteLine("done with client!");
 		}
-		private static bool IsClearedDeadStream(TcpClient c, int i, List<TcpClient> clients) {
-			if (c == null || !c.Connected) {
-				Console.WriteLine($"removing client {i} {c == null}");
-				if (clients != null) {
-					clients.RemoveAt(i);
+
+		public class ServerImplementation {
+			NetBuffer writeBuffer = new NetBuffer();
+			List<TcpClient> clients = new List<TcpClient>();
+			string animation = "/-\\|";
+			int animationIndex = 0;
+			int whenToAnimateNext = Environment.TickCount;
+
+			public async Task OnConnect(TcpClient newClient) {
+				NetworkStream stream = newClient.GetStream();
+				Console.WriteLine("connected to new client " + newClient.Client.RemoteEndPoint);
+				clients.Add(newClient);
+				// example serialization. TODO make this in a delegate, with the corresponding delegate for reading.
+				var message = $"DateTime: {DateTime.Now}";
+				var dateTimeBytes = Encoding.UTF8.GetBytes(message);
+				await stream.WriteAsync(dateTimeBytes);
+				Console.WriteLine($"Sent message: \"{message}\"");
+			}
+
+			public bool IsGood() {
+				if (Console.KeyAvailable) {
+					ConsoleKeyInfo keyInfo = Console.ReadKey();
+					if (keyInfo.Key == ConsoleKey.Escape) {
+						Console.WriteLine("~\n"); // needed because the console eats characters
+						return false;
+					}
+				}
+				int now = Environment.TickCount;
+				if (now >= whenToAnimateNext) {
+					Console.Write(animation[animationIndex++] + "\r");
+					if (animationIndex >= animation.Length) { animationIndex = 0; }
+					whenToAnimateNext = now + 200;
 				}
 				return true;
 			}
-			return false;
+
+			public async Task Update() {
+				await ReadClientsIntoBuffer();
+				await SendWriteBufferToClients();
+			}
+
+			private async Task ReadClientsIntoBuffer() {
+				for (int i = clients.Count - 1; i >= 0; --i) {
+					TcpClient client = clients[i];
+					if (ClearedDeadStreams(client, i)) { continue; }
+					await writeBuffer.Read(client);
+				}
+			}
+			public async Task SendWriteBufferToClients() {
+				if (writeBuffer.Count == 0) { return; }
+				Console.WriteLine($"writing {writeBuffer.Count} bytes: " +
+					Encoding.UTF8.GetString(writeBuffer.Buffer, 0, writeBuffer.Count));
+				for (int i = clients.Count - 1; i >= 0; --i) {
+					TcpClient client = clients[i];
+					if (ClearedDeadStreams(client, i)) { continue; }
+					await writeBuffer.WriteAndFlush(client);
+				}
+				writeBuffer.Clear();
+			}
+			private bool ClearedDeadStreams(TcpClient client, int i) {
+				if (client == null || !client.Connected) {
+					string status = client == null ? "deleted" : "disconnected";
+					Console.WriteLine($"removing client {i} ({status})");
+					if (clients != null) {
+						clients.RemoveAt(i);
+					}
+					return true;
+				}
+				return false;
+			}
 		}
 
+		public class ClientImplementation {
+			public TcpClient client;
+			public NetworkStream? stream;
+			public ValueTask<int> receivedTask;
+			public List<char> consoleInput = new List<char>();
+			public byte[] networkInputBuffer = new byte[1024]; // TODO use NetBuffer instead
+			public ClientImplementation() { }
+
+			public async Task OnConnect(TcpClient connectedClient) {
+				client = connectedClient;
+				Console.WriteLine("connected.");
+				stream = client.GetStream();
+				receivedTask = stream.ReadAsync(networkInputBuffer);
+			}
+
+			public bool IsGood() => client.Connected;
+
+			public async Task Update() {
+				if (Console.KeyAvailable) {
+					ConsoleKeyInfo keyInfo = Console.ReadKey();
+					switch (keyInfo.Key) {
+						case ConsoleKey.Enter: await FinishConsoleInput(); break;
+						case ConsoleKey.Escape: client.Close(); break;
+						default: AddConsoleInput(keyInfo.KeyChar); break;
+					}
+				}
+				if (stream.CanRead && receivedTask.IsCompleted) {
+					int received = receivedTask.Result;
+					if (received <= 0) {
+						Console.Error.WriteLine($"{received} bytes received");
+						return;
+					}
+					string message = Encoding.UTF8.GetString(networkInputBuffer, 0, received);
+					Console.WriteLine(message);
+					receivedTask = stream.ReadAsync(networkInputBuffer);
+				}
+			}
+
+			public async Task FinishConsoleInput() {
+				if (!stream.CanWrite) {
+					Console.WriteLine($"can't write '{string.Join("' '", consoleInput)}'");
+				} else if (consoleInput.Count > 0) {
+					byte[] outbytes = new byte[consoleInput.Count];
+					for (int i = 0; i < consoleInput.Count; ++i) {
+						outbytes[i] = (byte)consoleInput[i];
+					}
+					await stream.WriteAsync(outbytes);
+					stream.Flush();
+				}
+				consoleInput.Clear();
+			}
+
+			public void AddConsoleInput(char keyChar) {
+				if (keyChar == 0) { return; }
+				consoleInput.Add(keyChar);
+			}
+		}
+		/// <summary>
+		/// byte array with additional functionality for reading/writing to <see cref="TcpClient"/>
+		/// </summary>
 		public class NetBuffer {
 			/// <summary>
 			/// byte buffer for receiving and sending data
@@ -206,6 +256,11 @@ namespace networkingMinimal {
 			/// how much the buffer is filled
 			/// </summary>
 			private int _filled = 0;
+			/// <summary>
+			/// seperate buffer that reads data directly before being added to <see cref="_buffer"/>
+			/// </summary>
+			private byte[] _networkReadBuffer;
+			private int DefaultNetworkReadBufferSize = 1024;
 			public byte[] Buffer => _buffer;
 			public int Count => _filled;
 			public byte this[int i] { get { return _buffer[i]; } set { _buffer[i] = value; } }
@@ -218,51 +273,24 @@ namespace networkingMinimal {
 				_filled += count;
 			}
 			public void Clear() { _filled = 0; }
-			public async Task WriteFlush(List<TcpClient> clients) {
-				if (Count == 0) { return; }
-				Console.WriteLine($"writing {Count} bytes: " + System.Text.Encoding.UTF8.GetString(Buffer, 0, Count));
-				for (int i = clients.Count - 1; i >= 0; --i) {
-					if (IsClearedDeadStream(clients[i], i, clients)) { continue; }
-					await WriteFlush(clients[i]);
-				}
-				Clear();
-			}
-			public async Task WriteFlush(TcpClient c) {
+			public async Task WriteAndFlush(TcpClient c) {
 				NetworkStream ns = c.GetStream();
 				try {
 					await ns.WriteAsync(Buffer, 0, Count);
 					ns.Flush();
-				} catch {}
+				} catch { }
 			}
-			/// <param name="client"></param>
-			/// <param name="intermediateBuffer">where bytes being read directly from the socket go</param>
-			/// <returns></returns>
-			public async Task Read(TcpClient client, byte[] intermediateBuffer) {
+			public async Task Read(TcpClient client) {
 				NetworkStream ns = client.GetStream();
+				if (_networkReadBuffer == null) {
+					_networkReadBuffer = new byte[DefaultNetworkReadBufferSize];
+				}
 				while (ns.DataAvailable) {
-					int received = await ns.ReadAsync(intermediateBuffer);
+					int received = await ns.ReadAsync(_networkReadBuffer);
 					if (received <= 0) { return; }
-					Add(intermediateBuffer, received);
+					Add(_networkReadBuffer, received);
 				}
 			}
 		}
-
-		public class ClientCommunicator {
-			public TcpClient client;
-			public NetworkStream? stream;
-			public ValueTask<int> receivedTask;
-			public List<char> consoleInput = new List<char>();
-			public byte[] networkInputBuffer = new byte[1024];
-			public ClientCommunicator() {
-				client = new TcpClient();
-			}
-			public async Task ConnectAsync(IPEndPoint ipEndPoint) {
-				await client.ConnectAsync(ipEndPoint.Address, ipEndPoint.Port);
-				Console.WriteLine("connected.");
-				stream = client.GetStream();
-				receivedTask = stream.ReadAsync(networkInputBuffer);
-			}
-		}
-
 	}
 }
