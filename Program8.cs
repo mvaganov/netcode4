e0f52bd570edd7358d56431e2f0652a59f98c702
Michael Vaganov <mvaganov@hotmail.com>
2025-02-23 23:15:27 -0900

    simpler send/receive for socket, also fake http response

--- Program8.cs
+++ Program8.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +10,11 @@ namespace ChatProgram {
     public static void Main(string[] args) {
       IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
       Console.WriteLine("Have endpoint: " + endpoint);
+      string httpTimestamp = DateTime.UtcNow.ToString("ddd, dd MMM yyyy HH:mm:ss ") + "GMT";
+      Console.WriteLine(httpTimestamp);
 
-
-      string input = Console.ReadLine();
-      if (input == "s") {
+      char input = (char)Console.Read();
+      if (input == 's') {
         TcpListener listener = new TcpListener(endpoint);
         Console.WriteLine("Listener: " + listener);
         listener.Start();
@@ -23,12 +23,27 @@ namespace ChatProgram {
         Console.WriteLine("client connected: " + clientTask.Result.Client.RemoteEndPoint);
         NetworkStream clientStream = clientTask.Result.GetStream();
         byte[] inputBuffer = new byte[1024];
-        int receivedBytes = clientStream.Read(inputBuffer, 0, inputBuffer.Length);
-        Console.WriteLine("Received: " + Encoding.ASCII.GetString(inputBuffer, 0, receivedBytes));
-        Byte[] bytes = Encoding.ASCII.GetBytes("Hello World");
-        clientStream.Write(bytes, 0, bytes.Length);
-        clientStream.Flush();
-        Console.WriteLine("client stream completed");
+        bool pretendTobeHttpServer = true;
+        do {
+          if (clientStream.DataAvailable) {
+            int receivedBytes = clientStream.Read(inputBuffer, 0, inputBuffer.Length);
+            Console.WriteLine("Received: " + Encoding.ASCII.GetString(inputBuffer, 0, receivedBytes));
+            if (pretendTobeHttpServer) {
+              string content = "HELLO <b>WORLD</b>!";
+              string response200 = $"HTTP/1.1 200 OK\r\nDate: {httpTimestamp}\r\nServer: Apache/2.4.4 (Win32) OpenSSL/0.9.8y PHP/5.4.16\r\nLast-Modified: Sat, 30 Mar 2013 11:28:59 GMT\r\nETag: \"ca-4d922b19fd4c0\"\r\nAccept-Ranges: bytes\r\nContent-Length: {content.Length}\r\nKeep-Alive: timeout=5, max=100\r\nConnection: Keep-Alive\r\nContent-Type: text/html\r\n\r\n" +
+              content;
+              Byte[] ecode200 = Encoding.ASCII.GetBytes(response200);
+              clientStream.Write(ecode200, 0, ecode200.Length);
+              clientStream.Flush();
+            }
+          }
+          if (Console.KeyAvailable) {
+            input = Console.ReadKey(true).KeyChar;
+            byte[] sendLetter = { (byte)input };
+            clientStream.Write(sendLetter, 0, 1);
+            clientStream.Flush();
+          }
+        } while (input != 'q');
       } else {
         TcpClient tcpClient = new TcpClient();
         Console.WriteLine("Connecting");
@@ -40,10 +55,18 @@ namespace ChatProgram {
         networkStream.Flush();
         Console.WriteLine("Receiving message");
         byte[] inputBuffer = new byte[1024];
-        int receivedBytes = networkStream.Read(inputBuffer, 0, inputBuffer.Length);
-        // Task receivedBytesProcess = await networkStream.ReadAsync(inputBuffer, 0, inputBuffer.Length);
-        Console.WriteLine("Received");
-        Console.WriteLine("Received: " + Encoding.ASCII.GetString(inputBuffer, 0, receivedBytes));
+        do {
+          if (networkStream.DataAvailable) {
+            int receivedBytes = networkStream.Read(inputBuffer, 0, inputBuffer.Length);
+            Console.WriteLine("Received: " + Encoding.ASCII.GetString(inputBuffer, 0, receivedBytes));
+          }
+          if (Console.KeyAvailable) {
+            input = Console.ReadKey(true).KeyChar;
+            byte[] sendLetter = { (byte)input };
+            networkStream.Write(sendLetter, 0, 1);
+            networkStream.Flush();
+          }
+        } while (input != 'q');
       }
 
 
